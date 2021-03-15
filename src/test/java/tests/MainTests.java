9cb1debba8b54fcab889ba6fb0ab4bfc2dc65cc4
sergey <zendmir5@yandex.ru>
2021-03-15 21:53:04 +0300

    tests added

--- src/test/java/tests/MainTests.java
+++ src/test/java/tests/MainTests.java
@@ -1,19 +1,20 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class MainTests extends TestBase {
+    Random random = new Random();
+
     @Test
+    @DisplayName("Check language change")
     void changeLanguageTest() {
         step("Open main page", () -> {
             open("");
@@ -27,6 +28,7 @@ public class MainTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Pop up line should appear when scroll down")
     void popUpLineTest() {
         step("Open main page", () -> {
             open("");
@@ -40,6 +42,7 @@ public class MainTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Feedback form should open")
     void openFeedbackFormTest() {
         step("Open main page", () -> {
             open("");
@@ -53,6 +56,7 @@ public class MainTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Course program should be visible")
     void courseProgramTest() {
         step("Open main page", () -> {
             open("");
@@ -63,7 +67,7 @@ public class MainTests extends TestBase {
         step("Open random course", () -> {
             $("[data-group=\"all\"]")
                     .parent()
-                    .sibling(new Random().nextInt(6))
+                    .sibling(random.nextInt(6))
                     .click();
         });
         step("Check course program is visible", () -> {
@@ -72,14 +76,16 @@ public class MainTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Check news search")
     void newsSearchTest() {
         step("Open main page", () -> {
             open("");
         });
         step("Go to news page", () -> {
-            $(".nav__item-link").hover();
-            $(".nav-cat-wrap")
-                    .$("[href=\"https://arenadata.tech/about/news/\"]")
+            $("[href=\"/about/\"]").parent().hover();
+            $("[href=\"/about/\"]")
+                    .sibling(0)
+                    .$(byText("Новости"))
                     .click();
         });
         step("Search news", () -> {
@@ -91,14 +97,16 @@ public class MainTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Vacancy page should open")
     void showVacancyTest() {
         step("Open main page", () -> {
             open("");
         });
         step("Go to career page", () -> {
-            $(".nav__item-link").hover();
-            $(".nav-cat-wrap")
-                    .$("[href=\"https://arenadata.tech/about/career/\"]")
+            $("[href=\"/about/\"]").parent().hover();
+            $("[href=\"/about/\"]")
+                    .sibling(0)
+                    .$(byText("Карьера"))
                     .click();
         });
         step("Open vacancy", () -> {
@@ -106,6 +114,75 @@ public class MainTests extends TestBase {
         });
         step("Check vacancy is opened", () -> {
             $("h1").shouldHave(text("QA Lead"));
+        });
+    }
+
+    @Test
+    @DisplayName("Documentation should match selected product")
+    void productDocumentationTest() {
+        step("Open main page", () -> {
+            open("");
+        });
+        step("Open product", () -> {
+            $("[href=\"/products/\"]").parent().hover();
+            $("[href=\"/products/\"]")
+                    .sibling(0)
+                    .$(byText("Arenadata QuickMarts"))
+                    .click();
+        });
+        step("Open product documentation", () -> {
+            $("[href=\"https://docs.arenadata.io/adqm/\"]").click();
+        });
+        step("Documentation should match selected product", () -> {
+            switchTo().window(1);
+            $("#adqm")
+                    .$("p")
+                    .shouldHave(text("Arenadata QuickMarts"));
+        });
+    }
+
+    @Test
+    @DisplayName("Check email and phone number fields validation")
+    void feedbackFormValidationTest() {
+        step("Open main page", () -> {
+            open("");
+        });
+        step("Scroll to feedback form", () -> {
+            $("#main-feedback-form").scrollIntoView(true);
+        });
+        step("Set incorrect email and phone", () -> {
+            $("#main-feedback-form").$("[name=\"EMAIL\"]").setValue("test");
+            $("#main-feedback-form").$("[name=\"PHONE\"]").setValue("test").pressTab();
+        });
+        step("Check error message should be visible", () -> {
+            $("#main-feedback-form").$("[name=\"EMAIL\"]").shouldHave(attribute("aria-invalid", "true"));
+            $("#main-feedback-form").$("[name=\"PHONE\"]").setValue("test").shouldHave(attribute("aria-invalid", "true"));
+            $(".error-message").shouldBe(visible);
+        });
+        step("Set correct email and phone", () -> {
+            $("#main-feedback-form").$("[name=\"EMAIL\"]").setValue("test@test");
+            $("#main-feedback-form").$("[name=\"PHONE\"]").setValue("12345").pressTab();
+        });
+        step("Check error message should not be visible", () -> {
+            $("#main-feedback-form").$("[name=\"EMAIL\"]").shouldHave(attribute("aria-invalid", "false"));
+            $("#main-feedback-form").$("[name=\"PHONE\"]").shouldHave(attribute("aria-invalid", "false"));
+            $(".error-message").shouldNotBe(visible);
+        });
+    }
+
+    @Test
+    @DisplayName("Support advantage description should appear when hover")
+    void supportAdvantagesTest() {
+        step("Open main page", () -> {
+            open("");
+        });
+        step("Go to support page", () -> {
+            $("[data-id=\"cookie2\"]").click();
+        });
+        step("Check support advantage description should be visible", () -> {
+            int index = random.nextInt(8);
+            $$(".plus").get(index).hover();
+            $$(".plus__text").get(index).shouldBe(visible);
         });
     }
 }
